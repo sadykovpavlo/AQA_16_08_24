c100cfb1446821f3d8c30d4f8178be9a3f2ccb81
Pavlo Sadykov <sadykov@rozetka.ua>
2024-09-19 15:05:43 +0300

    changed assert method for tests

--- lesson_10/homework_10_test.py
+++ lesson_10/homework_10_test.py
@@ -7,21 +7,24 @@ class TestLogEvenFunction:
 
     def test_success_login_status_log(self):
         entry_data = ('User_1', 'success')
-        expected_data = ['User_1', 'success\n']
+        expected_data = ['User_1', 'success']
         log_event(*entry_data)
         result = log_file_return_last_log('login_system.log')
-        assert expected_data[0] in result, expected_data[1] in result
+        assert expected_data[0] in result and expected_data[1] in result, f"Expected {expected_data[0]} and " \
+                                                                          f"{expected_data[1]} not in {result}!"
 
     def test_expire_login_status_log(self):
         entry_data = ('User_1', 'expired')
-        expected_data = ['User_1', 'expired\n']
+        expected_data = ['User_1', 'expired']
         log_event(*entry_data)
         result = log_file_return_last_log('login_system.log')
-        assert expected_data[0] in result, expected_data[1] in result
+        assert expected_data[0] in result and expected_data[1] in result, f"Expected {expected_data[0]}  and " \
+                                                                          f"{expected_data[1]} not in {result}!"
 
     def test_another_login_status_log(self):
         entry_data = ('User_1', 'another')
-        expected_data = ['User_1', 'another\n']
+        expected_data = ['User_1', 'another']
         log_event(*entry_data)
         result = log_file_return_last_log('login_system.log')
-        assert expected_data[0] in result, expected_data[1] in result
+        assert expected_data[0] in result and expected_data[1] in result, f"Expected {expected_data[0]} and " \
+                                                                          f"{expected_data[1]} not in {result}!"
